03c8fa4aba65f6f069f7a06a1070101a2c9539fc
HMXHKAKA <97349507+Dreamkaka@users.noreply.github.com>
2024-10-22 15:58:21 +0800

    change rule

--- src/app/(app)/error.tsx
+++ src/app/(app)/error.tsx
@@ -14,11 +14,11 @@ export default ({ error, reset }: any) => {
 
   return (
     <NormalContainer>
-      <div className="flex min-h-[calc(100vh-10rem)] flex-col center">
+      <div className="center flex min-h-[calc(100vh-10rem)] flex-col">
         <h2 className="mb-5 text-center">
           <p>渲染页面时出现了错误</p>
           <p>
-            多次出现错误请联系开发者 <a href="mailto:i@innei.in">Innei</a>
+            多次出现错误请联系开发者 <a href="mailto:457970652@qq.com">晓寒</a>
             ，谢谢！
           </p>
         </h2>
